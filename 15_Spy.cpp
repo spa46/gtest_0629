9ec24a1e32ff902a7068f9be88ff7e1629bff8f1
Chansik Yun <chansigi@gmail.com>
2020-06-30 05:13:31 +0000

    .

--- 15_Spy.cpp
+++ 15_Spy.cpp
@@ -21,7 +21,7 @@ public:
 	
 	void Write(Level level, const std::string& message) {
 		for (DLoggerTarget* p : targets) {
-			p->Write(level, message);
+		// p->Write(level, message);
 		}
 	}
 };
@@ -33,7 +33,47 @@ public:
 //        만들어진 테스트 대역
 //        "다른 컴포턴트로부터의 간접 출력을 저장해두었다가 검증한다."
 
+#include <gtest/gtest.h>
+#include <algorithm>
 
+class SpyTarget : public DLoggerTarget {
+	std::vector<std::string> history;
+
+	std::string concat(Level level, const std::string& message) {
+		static const char* levelStr[] = {
+			"INFO", "WARN", "ERROR"
+		};
+
+		return std::string(levelStr[level]) + "@" + message;
+	}
+
+public:
+	void Write(Level level, const std::string& message) override {
+		history.push_back(concat(level, message));
+	}
+
+	// 테스트에서 확인할 수 있도록 하는 함수
+	bool IsReceived(Level level, const std::string& message) {
+		return std::find(history.begin(), history.end(),
+			concat(level, message)) != history.end();
+	}
+};
+
+// DLogger에 대해서 2개 이상의 타겟을 등록하고, Write를 호출하였을 때
+// 등록된 Target에 대해서 Write가 제대로 호출되었는지 여부를 검증하고 싶다.
+TEST(DLoggerTest, WriteTest) {
+	DLogger logger;
+	SpyTarget spy1, spy2;
+	logger.AddTarget(&spy1);
+	logger.AddTarget(&spy2);
+	Level test_level = INFO;
+	std::string test_message = "test_message";
+
+	logger.Write(test_level, test_message);
+
+	EXPECT_TRUE(spy1.IsReceived(test_level, test_message));
+	EXPECT_TRUE(spy2.IsReceived(test_level, test_message));
+}
 
 
 
