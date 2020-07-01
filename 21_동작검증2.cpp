6646c1581dcbf86a4125004314429700d459bc9e
Chansik Yun <chansigi@gmail.com>
2020-07-01 06:09:15 +0000

    .

--- 21_동작검증2.cpp
+++ 21_동작검증2.cpp
@@ -58,7 +58,7 @@ void Do2(User* user) {
 }
 
 // 호출 횟수를 검증할 때, 인자의 Matching에 따라 횟수가 변한다.
-TEST(UserTest, DoTest) {
+TEST(UserTest, DoTest2) {
 	MockUser mock;
 
 	EXPECT_CALL(mock, Add(_, 20)).Times(AtMost(3));
@@ -66,7 +66,42 @@ TEST(UserTest, DoTest) {
 	Do(&mock);
 }
 
+// EXPECT_CALL은 행위 기반 검증을 위해 사용합니다.
+// 결과를 제어하는 기능도 제공하고 있습니다.
+//  - 행위 기반 검증과 동시에 결과를 제어한다면, ON_CALL이 아닌
+//    EXPECT_CALL로 처리할 수 있습니다. 
+//      1. WillOnce
+//         : 한번에 호출에 대한 결과를 제어할 수 있습니다.
+//      2. WillRepeatedly
+//         : 기본 동작에 대한 결과를 제어할 수 있다.
 
+// EXPECT_CALL - 횟수 룰
+//  1. Times 정의하지 않으면, 1번을 가정한다.
+//  2. WillOnce()가 N개 있으면, N번을 가정한다.
+//  3. N개의 WillOnce()와 WillRepeatedly()가 있을 경우, AtLesat(N)을 가정한다.
+
+using testing::Return;
+
+TEST(UserTest, DoTest3) {
+	MockUser mock;
+
+	EXPECT_CALL(mock, Add(_, _))
+		.WillOnce(Return(101))
+		.WillOnce(Return(102))
+		.WillOnce(Return(103))
+		.WillOnce(Return(104))
+		.WillOnce(Return(105))
+		.WillRepeatedly(Return(300));
+
+	printf("%d\n", mock.Add(10, 30));
+	printf("%d\n", mock.Add(10, 30));
+	printf("%d\n", mock.Add(10, 30));
+	printf("%d\n", mock.Add(10, 30));
+	printf("%d\n", mock.Add(10, 30));
+	printf("%d\n", mock.Add(10, 30));
+	printf("%d\n", mock.Add(10, 30));
+	printf("%d\n", mock.Add(10, 30));
+}
 
 
 
